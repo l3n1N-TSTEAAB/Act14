4452b1f7302d7e24f187973841f6555cb4eaee54
l3n1N-TSTEAAB <lenin.gdeleon@gmail.com>
2025-08-08 21:22:49 -0600

    MENU

--- Act14.py
+++ Act14.py
@@ -7,6 +7,8 @@ def QuickSort(lista):
     iguales = [x for x in lista if x == pivote]
     mayores = [x for x in lista[1:] if x > pivote]
 
+    return QuickSort(menores) + iguales + QuickSort(mayores)
+
 def Busqueda():
     print("BUSQUEDA")
 
@@ -15,16 +17,35 @@ Diccionario = {}
 
 cantParticipantes = int(input("Ingrese la cantidad de participantes: "))
 cont = 0
+opcion = 0
 
-while(cont < cantParticipantes):
-    print("INGRESO DE PARTICIPANTES")
-    dorsal = int(input("NUMERO DE DORSAL: "))
-    Diccionario[dorsal]["NOMBRE"] = input("Ingrese el nombre: ")
-    Diccionario[dorsal]["Edad"] = int(input("Ingrese la edad: "))
-    Diccionario[dorsal]["CATEGORIA"] = input("Ingrese la categoria: ")
-    cont+=1
+while opcion != 4:
+    print("1. AGREGAR PARTICIPANTES")
+    print("2. MOSTRAR PARTICIPANTES ORDENADOS POR NOMBRE")
+    print("3. MOSTRAR PARTICIPANTES ORDENADOS POR EDAD")
+    print("4. SALIR")
+    opcion = int(input("OPCION A INGRESAR: "))
 
-lista = list(Diccionario.items())
-resultado = QuickSort(lista)
+    match(opcion):
+        case 1:
+          while (cont < cantParticipantes):
+            print("INGRESO DE PARTICIPANTES")
+            dorsal = int(input("NUMERO DE DORSAL: "))
+            Diccionario[dorsal]["NOMBRE"] = input("Ingrese el nombre: ")
+            Diccionario[dorsal]["Edad"] = int(input("Ingrese la edad: "))
+            Diccionario[dorsal]["CATEGORIA"] = input("Ingrese la categoria: ")
+            cont += 1
+        case 2:
+          lista = list(Diccionario.items())
+          resultado = QuickSort(lista)
+          for dorsal in resultado:
+            print(f"{dorsal}:[valor]")
+
+
+
+
+
+
+
 
 
